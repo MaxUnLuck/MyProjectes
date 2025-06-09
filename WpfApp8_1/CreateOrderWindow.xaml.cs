33cb64cf48eba9df427eb135b108d428c19167f9
Студенты <1214M-16@htk.lan>
2025-06-09 15:29:48 +0300

    Сохранил 8.1

---
+++ WpfApp8_1/CreateOrderWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp8_1
+{
+    /// <summary>
+    /// Логика взаимодействия для CreateOrderWindow.xaml
+    /// </summary>
+    public partial class CreateOrderWindow : Window
+    {
+        public CreateOrderWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AcceptButton_Копировать_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AcceptButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Сохранение
+            this.Close();
+        }
+    }
+}
---
+++ WpfApp8_1/GeneralWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp8_1
+{
+    /// <summary>
+    /// Логика взаимодействия для GeneralWindow.xaml
+    /// </summary>
+    public partial class GeneralWindow : Window
+    {
+        public GeneralWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void MenuSewOrder_Click(object sender, RoutedEventArgs e)
+        {
+            WindowSewOrders windowSewOrders = new WindowSewOrders();
+            windowSewOrders.Show();
+        }
+
+        private void MenuCreateOrder_Click(object sender, RoutedEventArgs e)
+        {
+            CreateOrderWindow windowCreateOrder = new CreateOrderWindow();
+            windowCreateOrder.Show();
+        }
+
+        private void MenuAboutProgram_Click(object sender, RoutedEventArgs e)
+        {
+            AboutProgramWindow windowAboutProgram = new AboutProgramWindow();
+            windowAboutProgram.Show();
+        }
+
+        private void MenuAdministration_Click(object sender, RoutedEventArgs e)
+        {
+            AdminUserControlWindow windowAdminUserControl = new AdminUserControlWindow();
+            windowAdminUserControl.Show();
+        }
+    }
+}
